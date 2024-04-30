d8f6e133ec2da99e481d29d230fb1792c7c404c2
jeroendoggen <jeroendoggen@gmail.com>
2024-04-30 17:08:56 +0200

    Update modbus-tcp.c
    Move forward declaration (after #include "config.h")

--- src/modbus-tcp.c
+++ src/modbus-tcp.c
@@ -55,12 +55,11 @@
 #define MSG_DONTWAIT MSG_NONBLOCK
 #endif
 
+// clang-format on
+#include "modbus-private.h"
 #ifdef HAVE_ACCEPT4
 extern int accept4 (int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len, int __flags);
 #endif
-// clang-format on
-
-#include "modbus-private.h"
 
 #include "modbus-tcp-private.h"
 #include "modbus-tcp.h"
